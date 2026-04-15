d11afb63dae91f0b7208203884f6f8282d3b8e02
Mostafa <mostafa.zabihi@web.de>
2026-04-15 16:31:25 +0200

    New project: Substring command in C#

---
+++ 04_Substring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Substring
+{
+    class Substring
+    {
+        public static void Main(String[] args)
+        {
+            string name = "Daniel Zacharin";
+            int charPos = name.IndexOf("Z");
+            string lastname = name.Substring(charPos);
+
+            Console.WriteLine(lastname);
+        }
+    }
+}
