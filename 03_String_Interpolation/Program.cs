ffd17f9a1ad2e59bdf55143ddb4b155a07efe8a6
Mostafa <mostafa.zabihi@web.de>
2026-04-15 15:21:50 +0200

    New project: String Interpolation

---
+++ 03_String_Interpolation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace String_Interpolation
+{
+    class Code_03
+    {
+        public static void Main(String[] args)
+        {
+            int num = 324;
+            Console.WriteLine($"The number is: {num}");
+        }
+    }
+}
