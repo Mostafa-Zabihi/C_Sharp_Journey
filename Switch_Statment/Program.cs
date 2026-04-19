b733f4f9245e72d463babe1b7557da848704281d
Mostafa <mostafa.zabihi@web.de>
2026-04-19 19:12:17 +0200

    Add switch expression project and practice string interpolation

---
+++ Switch_Statment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Switch_Statment{
+    class program
+    {
+        static void Main(String[] args)
+        {
+        string orderStatus = "Shipped";
+
+        string customerMessage = orderStatus switch
+        {
+            "Received" => "Your order has been received.",
+            "Processing" => "We are currently packing your items",
+            "Shipped" => "The package has been handed over to the carrier.",
+            "Delivered" => "The package was delivered successfuly",
+            _ => "Unknow status. Please contact our support team."
+        };
+
+        Console.WriteLine($"Customer Info: {customerMessage}");
+        }
+    }
+}
