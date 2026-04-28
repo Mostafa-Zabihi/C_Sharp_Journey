9811380c4e56030f1d0c8b717d6533e21794640d
Mostafa <mostafa.zabihi@web.de>
2026-04-28 21:07:45 +0200

    Add loop examples

---
+++ Loop_in_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace Loop_in_CSharp
+{
+    public class Car
+    {
+        public string[] BrandList = {"BMW", "Porsche", "Toyota", "Honda"};
+    }
+
+  public class Program
+    {
+          public static void Main(String[] args)
+        {
+           Car myCars = new Car();
+           foreach(string i in myCars.BrandList)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
