790b3c41441936b8f36e3ff8f76f412fb5597ef0
Mostafa <mostafa.zabihi@web.de>
2026-04-29 16:01:01 +0200

    Array learning in C#

---
+++ arrayInCSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace arrayInCSharp
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            string[] client = {"Daniel", "Musti", "Jashva", "Jamal", "Momo"};
+            Array.Sort(client);
+            foreach(string name in client)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
