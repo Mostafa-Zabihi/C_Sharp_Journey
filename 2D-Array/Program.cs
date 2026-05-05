3a5833ff3f7c103bac15fd28e76d84b1fb6bbd97
Mostafa <mostafa.zabihi@web.de>
2026-05-05 21:44:13 +0200

    2D-Array training

---
+++ 2D-Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Net.Sockets;
+namespace program
+{
+    class Two_D_Array
+    {
+        public static void Main(String[] args)
+        {
+            int[,] table = {{3, 23, 34, 342, 34},{99,24,532,634,3}};
+            foreach(double num in table)
+            {
+                Console.WriteLine(num);
+            }
+            
+        }
+
+        
+    }    
+}
